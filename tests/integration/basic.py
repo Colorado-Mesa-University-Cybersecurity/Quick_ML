9c39d97ee3f0fd7a65cca5e02129171972e15bc6
James Halladay <jehalladay@mavs.coloradomesa.edu>
2022-06-02 21:29:17 -0700

    basic tests to ensure that submodules are reachable from main module entrance point

--- tests/integration/basic.py
+++ tests/integration/basic.py
@@ -1,13 +1,17 @@
 '''
 Here we write tests that simply check to see if code is reachable from Quick/__init__.py
 '''
+
+# this bit of code allows us to import Quick while being in a different directory that 
+#    Quick is contained in
 import sys
-  
-# setting path
 sys.path.append('../../.')
+  
 
 
 
 import Quick
 
-Quick.hello()
+
+if(__name__ == "__main__"):
+    Quick.hello()
